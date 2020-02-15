676bae45f9d4682f8db9314e43a3f8afebe51412
Anton-Stechman <49045849+Anton-Stechman@users.noreply.github.com>
2020-02-15 22:28:22 +1300

    Add files via upload

---
+++ javascript/pageSelect.js
@@ -0,0 +1,42 @@
+const container = document.querySelector('.banner-left');
+const buttons   = Array.from(container.children);
+const displayPort = document.querySelector('.pagePort');
+
+var currentButton;
+
+//get the button with class .button-selected
+var getCurrentSelection = () => 
+{
+    return container.querySelector('.button-selected');
+}
+
+var getPage = () =>
+{
+   
+    return $(getCurrentSelection()).attr('value');
+}
+
+const loadPage = (page) => 
+{
+    $(displayPort).attr('src', page);
+}
+
+//Take input from button
+const selectButton = (button) =>
+{
+    //find the current button 
+    currentButton = getCurrentSelection();
+    
+    $(currentButton).removeClass('button-selected');
+    $(button).addClass('button-selected');
+    
+    //load page from button value
+    loadPage(getPage());
+}
+
+//initialize buttons, add button click function
+const button_init = (button, index) =>
+{
+    button.addEventListener('click', function() {selectButton(buttons[index]);});
+}
+buttons.forEach(button_init);
