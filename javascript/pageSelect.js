64fecccae1a5ff3c008b16004cb513b9c6aa0408
Anton-Stechman <49045849+Anton-Stechman@users.noreply.github.com>
2020-02-16 19:19:51 +1300

    staff login redirect added

--- javascript/pageSelect.js
+++ javascript/pageSelect.js
@@ -1,6 +1,6 @@
-const container = document.querySelector('.banner-left');
-const buttons   = Array.from(container.children);
-const displayPort = document.querySelector('.pagePort');
+const container     = document.querySelector('.banner-left');
+const buttons       = Array.from(container.children);
+const displayPort   = document.querySelector('.pagePort');
 
 var currentButton;
 
@@ -24,14 +24,21 @@ const loadPage = (page) =>
 //Take input from button
 const selectButton = (button) =>
 {
-    //find the current button 
-    currentButton = getCurrentSelection();
-    
-    $(currentButton).removeClass('button-selected');
-    $(button).addClass('button-selected');
-    
-    //load page from button value
-    loadPage(getPage());
+    if ($(button).attr('name') != "login") //change iframe for customer navigation
+    {
+        //find the current button 
+        currentButton = getCurrentSelection();
+
+        $(currentButton).removeClass('button-selected');
+        $(button).addClass('button-selected');
+
+        //load page from button value
+        loadPage(getPage());
+    }
+    else //go to staff login page
+    {
+        window.open(page, '_blank');
+    }
 }
 
 //initialize buttons, add button click function
